c80f593cfa2f6c07ea00ccacd3cdad3a8c322bd4
Narayan500 <147725461+Narayan500@users.noreply.github.com>
2023-10-31 22:26:43 +0530

    Add files via upload

---
+++ Conn.java
@@ -0,0 +1,19 @@
+package university.management.system;
+import java.sql.*;  
+public class Conn {
+    Connection c;
+    Statement s;
+    Conn(){  
+        try{  
+            Class.forName("com.mysql.cj.jdbc.Driver");  
+            c  =DriverManager.getConnection("jdbc:mysql:///universitymanagementsystem","root","Narayan@5874");    
+            s =c.createStatement(); 
+           
+          
+            
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}
+
